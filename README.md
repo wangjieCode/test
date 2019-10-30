8affdec361ab306a07d799e1e3eb3ed3a7be2a06
wjDome <1787612871@qq.com>
2019-10-30 13:50:55 +0800

    迭代器

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# javascript-
-## 记录js学习案例
+# 迭代器和生成器
+
+## 迭代器和迭代协议
+> 如果一个对象具有next方法，并且next方法满足一定的约束
+- value: any类型, 下一个数据的值
+- done: boolean类型, 是否已经迭代完成
+```javascript
+const iterator = {
+	next(){
+		return {
+			value:,
+			done:
+		}
+	}
+}
+```
--- index.js
+++ index.js
@@ -1,66 +1,13 @@
-function getProperty(obj, str) {
-	const arr = str.split(".");
-	let result = undefined;
-	for (let i = 0; i < arr.length; i++) {
-		obj = obj[arr[i]];
-		result = obj
-		if (result === undefined) {
-			return undefined
+const obj = {
+	next() {
+		return {
+			value: ,//下一个数据的值
+			done: ,//是否已经迭代完成
 		}
 	}
-	return result
 }
-var abj = {
-    a: {
-			b: {
-				c:1
-			}
-		}
+
+
+const o = {
+	[Symbol.iterator]
 }
-console.log( getProperty(abj,"a.b.y.x"))
-
-
-
-
-function Observer() {
-    this.arr = {};
-}
-Observer.prototype.listen = function (ele, cb) {
-	let r = ele
-  this.arr[r] = cb;
-};
-Observer.prototype.trigger = function () {
-		for( let i = 0; i < arguments.length; i ++){
-			console.log(
-			this.arr[ arguments[i] ]()
-			)
-		}
-}
-Observer.prototype.remove = function (ele, cb) {
-    for( var prop in this.arr ){
-        if( ele === prop ) {
-            delete this.arr[prop];
-            cb()
-        }
-    }
-}
-Array.prototype.multiply = function() {
-    var arr = [];
-    for (var i = 0; i < this.length; i ++ ){
-        arr.push( this[i] * this[i] );
-    }
-    return arr;
-}
-let r = new Observer();
-// r.listen("mmp", () => console.log("mmp") );
-// r.trigger("mmp")
-// console.log(r)
-
-
-
-
-
-var a = [1, 2, 3, 4, 5];
-a.multiply();
-// console.log(a);
-// [1, 2, 3, 4, 5, 1, 4, 9, 16, 25]
